64f3aa1e7f12916e4808b7e42da5d9b112522271
MattW <mattnwarnock@yahoo.co.uk>
2021-09-22 15:26:20 +0100

    Pass method test

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,5 +3,9 @@
 require 'docking_station'
 
 RSpec.describe DockingStation do
+  it 'reponse to release bike method' do
+    docking_station = DockingStation.new
 
+    expect(docking_station).to respond_to(:release_bike)
+  end
 end
