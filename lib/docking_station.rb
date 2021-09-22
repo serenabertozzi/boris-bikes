f189d46e39633077d13b804d12c4e736281eb135
MattW <mattnwarnock@yahoo.co.uk>
2021-09-22 17:21:28 +0100

    Pass test on working bike

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,4 +1,7 @@
+# frozen_string_literal: true
+
 class DockingStation
   def release_bike
+    Bike.new
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -8,4 +8,23 @@ RSpec.describe DockingStation do
 
     expect(docking_station).to respond_to(:release_bike)
   end
+
+  describe '#release_bike' do
+    it 'returns a bike object' do
+      docking_station = DockingStation.new
+
+      result = docking_station.release_bike
+
+      expect(result).to be_a_kind_of(Bike)
+    end
+  end
+
+  it 'expects bike to be working' do
+    docking_station = DockingStation.new
+    bike = docking_station.release_bike
+
+    result = bike.working?
+
+    expect(result).to eq true
+  end
 end
