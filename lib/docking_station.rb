b049e5e6ced9d460cbc07f0093783eb6dfae6770
Serena Bertozzi <serenabertozzi@Serenas-MacBook-Air.local>
2021-09-23 15:15:17 +0100

    add instance variable

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,9 +1,16 @@
 # frozen_string_literal: true
 
-require_relative 'bike'
+require_relative "bike"
 
 class DockingStation
+  attr_reader :bike
+
   def release_bike
     Bike.new
+    # @bike = bike
+  end
+
+  def dock(bike)
+    @bike = bike
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,12 +1,22 @@
 # frozen_string_literal: true
 
-require 'docking_station'
+require "docking_station"
 
 RSpec.describe DockingStation do
-  it { is_expected.to respond_to :release_bike}
+  it { is_expected.to respond_to :release_bike }
 
-  it 'returns a bike object' do
+  it "returns a bike object" do
     bike = subject.release_bike
     expect(bike).to be_working
+  end
+
+  it { is_expected.to respond_to(:dock).with(1).argument }
+
+  it { is_expected.to respond_to(:bike) }
+
+  it "docks a bike in the docking station" do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to(eq(bike))
   end
 end
